c1da85b23c5e6399e93eea1f68ab05bba8ca018b
Robert Gleason <l2ob122@gmail.com>
2025-03-10 22:10:37 -0700

    Add admin sidebar component and related icons for dashboard navigation

---
+++ src/components/dashboard/sidebar/nav-admin.tsx
@@ -0,0 +1,10 @@
+import { DashboardSidebarMenuInterface } from '@/lib/types';
+
+interface SidebarNavAdminProps {
+  menuLinks: DashboardSidebarMenuInterface[];
+}
+
+const SidebarNavAdmin = ({ menuLinks }: SidebarNavAdminProps) => {
+  return <div>{}</div>;
+};
+export default SidebarNavAdmin;
---
+++ src/constants/icons.ts
@@ -0,0 +1,83 @@
+import {
+  DashboardIcon,
+  StoreIcon,
+  CreateStoreIcon,
+  BoxListIcon,
+  SettingsIcon,
+  BoxesIcon,
+  CategoriesIcon,
+  ThreeBoxesIcon,
+  ProductsIcon,
+  InventoryIcon,
+  CouponIcon,
+  ShippingIcon,
+  OfferIcon,
+} from '@/components/dashboard/icons';
+
+export const icons = [
+  {
+    label: 'Dashboard',
+    value: 'dashboard',
+    path: DashboardIcon,
+  },
+  {
+    label: 'Store',
+    value: 'store',
+    path: StoreIcon,
+  },
+  {
+    label: 'Create Store',
+    value: 'create-store',
+    path: CreateStoreIcon,
+  },
+  {
+    label: 'Box List',
+    value: 'box-list',
+    path: BoxListIcon,
+  },
+  {
+    label: 'Settings',
+    value: 'settings',
+    path: SettingsIcon,
+  },
+  {
+    label: 'Boxes',
+    value: 'boxes',
+    path: BoxesIcon,
+  },
+  {
+    label: 'Categories',
+    value: 'categories',
+    path: CategoriesIcon,
+  },
+  {
+    label: 'Collections',
+    value: 'collections',
+    path: ThreeBoxesIcon,
+  },
+  {
+    label: 'Products',
+    value: 'products',
+    path: ProductsIcon,
+  },
+  {
+    label: 'Inventory',
+    value: 'inventory',
+    path: InventoryIcon,
+  },
+  {
+    label: 'Coupons',
+    value: 'coupons',
+    path: CouponIcon,
+  },
+  {
+    label: 'Shipping',
+    value: 'shipping',
+    path: ShippingIcon,
+  },
+  {
+    label: 'Offers',
+    value: 'offers',
+    path: OfferIcon,
+  },
+];
