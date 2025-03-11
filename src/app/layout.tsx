95aed04ad02120998f984b8ba6ca27f1390f5ac4
Robert Gleason <l2ob122@gmail.com>
2025-03-10 18:32:41 -0700

    Integrate Clerk for user authentication and update layout with ClerkProvider, add UserButton to Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter, Barlow } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from 'next-themes';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 const barlow = Barlow({
@@ -22,17 +23,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className} ${barlow.variable}`}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} ${barlow.variable}`}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import ThemeToggle from '@/components/shared/theme-toggle';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
-      <div className='w-100 flex justify-end'>
+      <div className='w-100 flex justify-end gap-4'>
+        <UserButton />
         <ThemeToggle />
       </div>
+      <h1>Home</h1>
     </div>
   );
 }
